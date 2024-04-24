3f413a9ae5beb35b96fbce51c64636cfa8298e6d
LazarosZiskos <ziskoslaz@gmail.com>
2024-04-24 14:19:54 +0300

    shorting functionality

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -6,22 +6,26 @@ import NftCard from "../home/NftCard.jsx";
 const ExploreItems = () => {
   const [exploreNfts, setExploreNfts] = useState([]);
   const [renderedItems, setRenderedItems] = useState(8);
+  const [shortValue, setShortValue] = useState("");
 
   const getData = async () => {
     const { data } = await axios.get(
-      "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${shortValue}`
     );
-    console.log(data);
     setExploreNfts(data);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [shortValue]);
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          defaultValue=""
+          onChange={(e) => setShortValue(e.target.value)}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
